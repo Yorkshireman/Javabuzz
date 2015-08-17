7fc563a8734e8bd0ae91639d39523dc63062bbbf
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-17 20:10:20 +0100

    write tests for divisible by 5 and divisible by 15 and make them pass

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -6,15 +6,37 @@ describe('Javabuzz', function() {
 
   describe('knows when a number is', function() {
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
 
   describe('knows when a number is NOT', function(){
     it('divisible by 3', function(){
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(4)).toBe(false);
+    });
+  });
+
+  describe('knows when a number is', function(){
+    it('divisible by 5', function(){
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function(){
+    it('divisible by 5', function(){
+      expect(javabuzz.isDivisibleByFive(11)).toBe(false);
+    });
+  });
+
+  describe('knows when a number is', function(){
+    it('divisible by 3 AND 5', function(){
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function(){
+    it('divisible by 3 and 5', function(){
+      expect(javabuzz.isDivisibleByFifteen(10)).toBe(false);
     });
   });
 });
